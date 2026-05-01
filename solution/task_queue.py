6fab6db488c3a849f724e5d80ac4b4f5e79f5748
Sushma Nalamaru <susheester@gmail.com>
2026-05-01 15:53:26 -0500

    Add TaskQueue class for asynchronous task handling
    Implemented a TaskQueue class with queue and worker methods to handle tasks asynchronously.

--- solution/task_queue.py
+++ solution/task_queue.py
@@ -1,1 +1,21 @@
+import queue
+import threading
+import time
 
+class TaskQueue:
+    def __init__(self):
+        self.q = queue.Queue()
+
+        # start one worker- using queue and worker thread
+        threading.Thread(target=self._worker, daemon=True).start()
+
+    def enqueue(self, handler, payload):
+        print(f"[{time.strftime('%H:%M:%S')}] Task enqueued")
+        self.q.put((handler, payload))
+
+    def _worker(self):
+        while True:
+            handler, payload = self.q.get()
+            print(f"[{time.strftime('%H:%M:%S')}] Task started")
+            handler(payload)
+            print(f"[{time.strftime('%H:%M:%S')}] Task finished")
